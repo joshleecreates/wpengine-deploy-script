4132f19fae4e49feec06cc737797f60b12b06201
Josh Lee <josh@daftlabs.com>
2015-05-04 15:44:57 -0400

    trying to fix

--- wpengine.sh
+++ wpengine.sh
@@ -1,20 +1,7 @@
-environment=$1
-theme="sage"
-
+endpoint=$1
 
 echo "Starting the build process..."
-if [ "$environment" == "staging" ]
-then
-  git checkout develop &> /dev/null
-  gulp &> /dev/null
-elif [ "$environment" == "production" ]
-then
-  git checkout master &> /dev/null
-  gulp --production &> /dev/null
-else
-  echo "Invalid environment."
-  exit
-fi
+grunt build &> /dev/null
 
 if [[ -n $(git status -s) ]]
 then
@@ -27,16 +14,10 @@ if [ -n "$exists" ]
 then
   git branch -D wpengine &> /dev/null
 fi
-git checkout -b wpengine &> /dev/null
+git checkout --orphan wpengine &> /dev/null
 
-cp -r web/app wp-content &> /dev/null
-rm "wp-content/themes/${theme}/.gitignore" &> /dev/null
-rm "wp-content/mu-plugins/bedrock-autoloader.php" &> /dev/null
-rm "wp-content/mu-plugins/disallow-indexing.php" &> /dev/null
-rm "wp-content/mu-plugins/register-theme-directory.php" &> /dev/null
 rm .gitignore &> /dev/null
 echo "/*\n!wp-content/\nwp-content/uploads" >> .gitignore
-git ls-files | xargs git rm --cached &> /dev/null
 
 cd wp-content/
 find . | grep .git | xargs rm -rf
@@ -44,16 +25,11 @@ cd ../
 
 git add . &> /dev/null
 git commit -am "WPEngine build from: $(git log -1 HEAD --pretty=format:%s)$(git rev-parse --short HEAD 2> /dev/null | sed "s/\(.*\)/@\1/")" &> /dev/null
+
+git remote add wpengine $endpoint
+
 echo "Pushing to WPEngine..."
-if [ "$environment" == "staging" ]
-then
-  git push staging wpengine:master --force &> /dev/null
-  git checkout develop &> /dev/null
-elif [ "$environment" == "production" ]
-then
-  git push production wpengine:master --force &> /dev/null
-  git checkout master &> /dev/null
-fi
+git push wpengine wpengine:master --force &> /dev/null
+
 git branch -D wpengine &> /dev/null
-rm -rf wp-content/ &> /dev/null
 echo "Successfully deployed."
