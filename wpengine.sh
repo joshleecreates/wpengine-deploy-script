8f35cb468125f0f3ff98e2c422ac7b171ad9dc6f
Josh Lee <josh@daftlabs.com>
2015-05-04 16:39:33 -0400

    makes more compatible with daft labs style repos. works!

--- wpengine.sh
+++ wpengine.sh
@@ -14,22 +14,25 @@ if [ -n "$exists" ]
 then
   git branch -D wpengine &> /dev/null
 fi
-git checkout --orphan wpengine &> /dev/null
 
-rm .gitignore &> /dev/null
+git checkout --orphan wpengine &> /dev/null
+git rm -rf --cached .
+
+rm .gitignore
 echo "/*\n!wp-content/\nwp-content/uploads" >> .gitignore
 
 cd wp-content/
 find . | grep .git | xargs rm -rf
 cd ../
 
-git add . &> /dev/null
-git commit -am "WPEngine build from: $(git log -1 HEAD --pretty=format:%s)$(git rev-parse --short HEAD 2> /dev/null | sed "s/\(.*\)/@\1/")" &> /dev/null
+git add .
+git commit -am "WPEngine build from: $(git log -1 HEAD --pretty=format:%s)$(git rev-parse --short HEAD 2> /dev/null | sed "s/\(.*\)/@\1/")"
 
-git remote add wpengine $endpoint
+git remote add wpengine $endpoint &> /dev/null
 
 echo "Pushing to WPEngine..."
-git push wpengine wpengine:master --force &> /dev/null
+git push wpengine wpengine:master --force
 
-git branch -D wpengine &> /dev/null
+git checkout master
+git branch -D wpengine
 echo "Successfully deployed."
